55da262a50f1184bee03a15d35a72ef3f60b2464
shinmyoungha <audgk0309@naver.com>
2025-03-18 09:32:15 +0900

    이름 수정 예제

--- jdbc1/src/edu/kh/jdbc/JDBCExample6.java
+++ jdbc1/src/edu/kh/jdbc/JDBCExample6.java
@@ -17,15 +17,17 @@ public class JDBCExample6 {
 		// 2. commit/rollback 처리하기
 		// 3. 성공 시 "수정 성공!" 출력 / 실패 시 "아이디 도는 비밀번호 불일치" 출력
 		
+		// 1) JDBC 객체 참조변수 선언 + 키보드 입력용 객체 sc 선언
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		
 		// SELECT 가 아니기 때문에 ResultSet 필요 없음!
 		
 		Scanner sc = null;
+		
 		try {
 			// 2. DriverManager 객체를 이용해서 Connection 객체 생성
-			// 2-1) Oracle JDBC Driver 객체 메모리 로드
+			// 2-1) OracleDriver 객체 메모리 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 						
 			// 2-2) DB 연결 정보 작성
@@ -36,7 +38,9 @@ public class JDBCExample6 {
 			// 2-3) DB 연결정보와 DriverManager 를 이용해서 Connection 생성
 			conn = DriverManager.getConnection(url,	userName, password);
 		
-			// 3. SQL 작성
+			// 3. SQL 작성 + AutoCommit 끄기
+			conn.setAutoCommit(false);
+			
 			sc = new Scanner(System.in);
 			
 			System.out.print("아이디 입력 : ");
@@ -45,46 +49,40 @@ public class JDBCExample6 {
 			System.out.print("비밀번호 입력 : ");
 			String pw = sc.nextLine();
 			
-			System.out.print("이름 입력 : ");
+			System.out.print("수정할 이름 입력 : ");
 			String name = sc.nextLine();
 			
 			String sql = """
-					INSERT INTO TB_USER 
-					VALUES(SEQ_USER_NO.NEXTVAL, ?, ?, ?, DEFAULT )
+					UPDATE TB_USER SET 
+					USER_NAME = ?
+					WHERE USER_ID = ?
+					AND USER_PW = ?
 					""";
 			
 			// 4. PreparedStatement 객체 생성
-			// -> 객체 생성과 동시에 SQL이 담겨지게 됨
-			// -> 미리 ?(위치홀더)에 값을 받을 준비를 해야되기 때문에..
-			
-			//stmt = conn.createStatement();
-			//stmt.executeQuery(sql);
-			
 			pstmt = conn.prepareStatement(sql);
 			
-			// 5. ? 위치홀거 알맞은 값 대입
+			// 5. ? 위치홀더 알맞은 값 대입
 			// pstmt.set자료형(?순서, 대입할 값)
-			pstmt.setString(1, id);
-			pstmt.setString(2, pw);
-			pstmt.setString(3, name);
+			pstmt.setString(1, name);
+			pstmt.setString(2, id);
+			pstmt.setString(3, pw);
 			// -> 여기까지 작성하면 SQL 완료된 상태!
 			
-			// + DML 수행전에 해줘야 할 것!
-			// AutoCommit 끄기!
-			// -> 왜 끄는건가? 개발자가 트랜잭션을 마음대로 제어하기 위해서
-			conn.setAutoCommit(false);
+			
 			
 			// 6. SQL(INSERT) 수행 후 결과(int) 반환 받기
 			// executeQuery() : SELECT 수향, ResultSet 반환
 			// executeUpdate() : DML 수행, 결과 행 갯수(int) 반환
 			// -> 보통 DML 실패 0, 성공 시 0 초과된 값이 반환된다
 			
-			// pstmt 에서 executeQuery(매개변수), executeUpdate(매개변수) 매개변수 자리에 아무것도 없어야 한다!
+			// pstmt 에서 executeQuery(매개변수), 
+			// executeUpdate(매개변수) 매개변수 자리에 아무것도 없어야 한다!
 			int result = pstmt.executeUpdate();
 			
-			// 7. result 값에 따른 결과 처리 + 트랜잭션 제어처리
+			// 7. result 값에 따른 결과 처리 + commit/rollback
 			if(result > 0) { // INSERT 성공 시
-				System.out.println(name + "님이 추가 되었습니다.");
+				System.out.println("수정 성공!");
 				conn.commit(); // INSERT 성공 시 COMMIT 수행 -> DB에 INSERT 영구 반영
 				
 			} else { // INSERT 실패
